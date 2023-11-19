b3c4c6736d83a29cc137267761e88ccf7dc0cdb4
Garos1103 <119961634+Garos1103@users.noreply.github.com>
2023-11-19 21:36:48 +0300

    Add files via upload

---
+++ hw4.sh
@@ -0,0 +1,14 @@
+import paramiko
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+hostname = 'remote_host'
+port = 22
+ssh.connect(hostname, port, username='username', password='password')
+stdin, stdout, stderr = ssh.exec_command('command_to_execute')
+
+output = stdout.read().decode()
+
+print('Comand Output:, output')
+
+print('Command Error', error)
